d27084f06c88f9d5f5e13febae79c11c8e4993d9
ManuDeveloper <manuepeva@gmail.com>
2020-08-10 12:47:42 -0500

    Updates

---
+++ precache-manifest.a7f564dd43400be0f5a53daf980e28ca.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f039c88eadb9c2a8e6a9c2f29bf20e",
+    "url": "/CSS-Grid-and-React/index.html"
+  },
+  {
+    "revision": "a6a2f5b4419ef367a0a7",
+    "url": "/CSS-Grid-and-React/static/css/main.46b0201e.chunk.css"
+  },
+  {
+    "revision": "3a75f04eb1ec6332b962",
+    "url": "/CSS-Grid-and-React/static/js/2.72df24f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CSS-Grid-and-React/static/js/2.72df24f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a2f5b4419ef367a0a7",
+    "url": "/CSS-Grid-and-React/static/js/main.a1322c10.chunk.js"
+  },
+  {
+    "revision": "e7f16ae012cd709ed9ae",
+    "url": "/CSS-Grid-and-React/static/js/runtime-main.87fb3660.js"
+  },
+  {
+    "revision": "252acbc5581e6ec5cbc92ae335906695",
+    "url": "/CSS-Grid-and-React/static/media/image1.252acbc5.jpg"
+  },
+  {
+    "revision": "d065d5ff9f73b7f219c60f8227e79cd0",
+    "url": "/CSS-Grid-and-React/static/media/image10.d065d5ff.jpg"
+  },
+  {
+    "revision": "bdce1989064333a55f258bb2ca5ce63c",
+    "url": "/CSS-Grid-and-React/static/media/image11.bdce1989.jpg"
+  },
+  {
+    "revision": "a548d083c759dd39d26125fb040a9c4a",
+    "url": "/CSS-Grid-and-React/static/media/image12.a548d083.jpg"
+  },
+  {
+    "revision": "e951768fb7bf646d715b37fee33b67ac",
+    "url": "/CSS-Grid-and-React/static/media/image2.e951768f.jpg"
+  },
+  {
+    "revision": "5170405261fdb7ae4a99e245dc7407cf",
+    "url": "/CSS-Grid-and-React/static/media/image3.51704052.jpg"
+  },
+  {
+    "revision": "ddfde60442538939d67f30e73efa0b17",
+    "url": "/CSS-Grid-and-React/static/media/image4.ddfde604.jpg"
+  },
+  {
+    "revision": "8389249b1684df3977226cb6ad967cc2",
+    "url": "/CSS-Grid-and-React/static/media/image5.8389249b.jpg"
+  },
+  {
+    "revision": "f612749f38946cb36c8e93679b741f68",
+    "url": "/CSS-Grid-and-React/static/media/image6.f612749f.jpg"
+  },
+  {
+    "revision": "2074650fdd8c82985f3c328005bc5f53",
+    "url": "/CSS-Grid-and-React/static/media/image7.2074650f.jpg"
+  },
+  {
+    "revision": "4a8555a1223cbdbb860fc730276fc4d9",
+    "url": "/CSS-Grid-and-React/static/media/image8.4a8555a1.jpg"
+  },
+  {
+    "revision": "c5280cbdce44d6d436d6c11c283a9ebc",
+    "url": "/CSS-Grid-and-React/static/media/image9.c5280cbd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CSS-Grid-and-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
